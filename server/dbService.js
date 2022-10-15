f6ea47e8fa000d25dfab82a19658eef8f5520d00
Djradek2 <112021889+Djradek2@users.noreply.github.com>
2022-10-15 19:54:41 +0200

    Add files via upload
    Unfinished server files

---
+++ server/dbService.js
@@ -0,0 +1,63 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+let instance = null;
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.SERVER_IP,
+    PORT: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSW,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err)=> {
+    if (err) {
+        console.log(err.message);
+    }
+    console.log('db ' + connection.state);
+})
+
+class DbService{
+    static getDbServiceInstance(){
+        return instance ? instance : new DbService();                   //vrátí instanci pokud existuje, jinak ji vytvoří, (Condition ? True : False)
+    }
+
+    async getLine(name){
+        console.log(name);
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM `words` WHERE id=(?);"
+                connection.query(query,[name], (err,result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result);
+                })
+            });
+            return response;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getAllData(){
+        try {
+            const response = await new Promise((resolve, reject) => {   //Promise = něco udělá, čeká na návrat proměných (resolve/reject)
+                const query = "SELECT * FROM words;";
+
+                connection.query(query, (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result);
+                }) 
+            });
+
+            //console.log(response);
+            return response;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = DbService;
---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const env = require('dotenv');
+const dbService = require('./dbService');
+env.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+//const db = dbService.getDbServiceInstance();
+
+app.post('/get', (request, response) => {  
+    var name = request.body;
+    name = name.lol;
+    console.log(name);   
+    const db = dbService.getDbServiceInstance();
+    const result = db.getLine(name);
+    result
+    .then(word => response.json({word: word}))
+    .catch(err => console.log(err));
+});
+
+app.get('/getAll', (request, response) => {         //function for getting all SQL entries
+    const db = dbService.getDbServiceInstance();
+    
+    const result = db.getAllData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+});
+
+app.post('/send', (request, response) => {
+    var name = request.body;
+});
+
+app.listen(process.env.SERVER_PORT, () => console.log('server online'));
